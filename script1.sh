253e2e495d677e3899d139a60c8cedfa310af381
Diego <diego.pedroche@hotmail.com>
2025-10-29 19:54:28 +0100

    vpc y subnet listas

--- script1.sh
+++ script1.sh
@@ -1,5 +1,5 @@
 #creo la vpc y devuelvo su id
-VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.1.0/24 \
     --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=vpcdiego}]' \
     --query Vpc.VpcId --output text)
 
@@ -10,3 +10,17 @@ echo $VPC_ID
 aws ec2 modify-vpc-attribute \
     --vpc-id $VPC_ID \
     --enable-dns-hostnames "{\"Value\":true}"
+
+#creo la subnet y devuelvo su id
+SUB_ID=$(aws ec2 create-subnet \
+    --vpc-id $VPC_ID \
+    --cidr-block 192.168.1.0/28 \
+    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=mi-subred1-diego}]' \
+    --query Subnet.SubnetId --output text)
+
+#muestra el id de la subnet
+echo $SUB_ID
+
+#habilito la asignacion de ipv4 publica en la subred
+#comprobar NO se habilita y tenemos que hacerlo a posteriori
+aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
