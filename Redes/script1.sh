d0d3e4d02ca4e8bc4a7500deaf27f0f4765ee1cc
Diego <diego.pedroche@hotmail.com>
2025-11-05 18:50:42 +0100

    añado el gs al ec2

--- Redes/script1.sh
+++ Redes/script1.sh
@@ -4,6 +4,7 @@ VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.1.0/24 \
     --query Vpc.VpcId --output text)
 
 #muestro la vpcid que he creado
+echo id de la vpc
 echo $VPC_ID
 
 #habilitar dns en la vpc
@@ -19,6 +20,7 @@ SUB_ID=$(aws ec2 create-subnet \
     --query Subnet.SubnetId --output text)
 
 #muestra el id de la subnet
+echo Id de la subnet
 echo $SUB_ID
 
 #habilito la asignacion de ipv4 publica en la subred
@@ -32,9 +34,10 @@ SG_ID=$(aws ec2 create-security-group --vpc-id $VPC_ID \
     --query GroupId \
     --output text)
 
+echo Id del grupo de seguridad
 echo $SG_ID
 
-#modifico 
+#modifico las reglas de entrada del grupo de seguridad
 aws ec2 authorize-security-group-ingress \
     --group-id $SG_ID \
     --ip-permissions '[{"IpProtocol":"tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "0.0.0.0/0", "Description": "AllowHTTP"}]}]'
@@ -48,13 +51,12 @@ EC2_ID=$(aws ec2 run-instances \
     --instance-type t2.micro \
     --key-name vockey \
     --subnet-id $SUB_ID \
+    --security-group-ids $SG_ID \
     --associate-public-ip-address \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=miec2}]' \
     --query Instances.InstanceId --output text)
 
 #muestro la id de la EC2
 sleep 15
+echo Id de la instancia
 echo $EC2_ID
-
-
-
